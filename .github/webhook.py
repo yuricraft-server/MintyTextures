523ba270071534b18a15f1ec9ef43b3753732983
mintcolon3 <143480965+mintcolon3@users.noreply.github.com>
2025-11-01 16:45:27 +0000

    close,,

--- .github/webhook.py
+++ .github/webhook.py
@@ -58,6 +58,7 @@ def main():
     payload = {
         "username": "Yuri Inspector",
         "avatar_url": "https://cdn.discordapp.com/avatars/1427680032305971300/1fe529c06f7534ce9a30ceacd5c63c08.png?size=1024",
+        "content": "bwaa",
         "flags": 32768,
         "components": [
             {
